d66c2db31e382c7a6052913eda0547ae6920d0f9
Ahmettcakar <116648416+Ahmettcakar@users.noreply.github.com>
2025-12-29 13:14:53 +0300

    first commit

---
+++ basic_list/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(home: BasicList()));
+}
+
+class BasicList extends StatelessWidget {
+  const BasicList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
